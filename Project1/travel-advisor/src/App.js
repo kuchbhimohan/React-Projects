ba4953abf9f76008429db6f0fada5d277286f9ed
Rishabh Jain <jainrishabh71011@gmail.com>
2022-03-18 17:55:09 +0530

    changed on 18th March 2022

--- Project1/travel-advisor/src/App.js
+++ Project1/travel-advisor/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { createFactory } from "react";
+import {CssBaseline,Grid} from '@material-ui/core'
+import Header from "./components/Header/Header";
+import Lists from "./components/Lists/Lists";
+import Map from "./components/Map/Map";
+// import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+const App = () => {
+  return ( 
+      <>
+        <CssBaseline/>
+        <Header/>
+        <Grid consider spacing={3} style={{ width : '100%' }}>
+          <Grid item xs = {12} md = {4}>
+            <Lists/>
+          </Grid>
+          <Grid item xs = {12} md = {8}>
+            <Map/>
+          </Grid>
+        </Grid>
+      </>
+   );
 }
-
 export default App;
+
