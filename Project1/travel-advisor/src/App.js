fd6c24e87853e2e67e6a2f1e459c610b202ed1ba
Rishabh Jain <jainrishabh71011@gmail.com>
2022-03-19 22:09:36 +0530

    changed on 19th March 2022

--- Project1/travel-advisor/src/App.js
+++ Project1/travel-advisor/src/App.js
@@ -1,5 +1,5 @@
 import React, { createFactory } from "react";
-import {CssBaseline,Grid} from '@material-ui/core'
+import { CssBaseline, Grid } from '@material-ui/core'
 import Header from "./components/Header/Header";
 import Lists from "./components/Lists/Lists";
 import Map from "./components/Map/Map";
@@ -7,20 +7,62 @@ import Map from "./components/Map/Map";
 
 
 const App = () => {
-  return ( 
-      <>
-        <CssBaseline/>
-        <Header/>
-        <Grid consider spacing={3} style={{ width : '100%' }}>
-          <Grid item xs = {12} md = {4}>
-            <Lists/>
-          </Grid>
-          <Grid item xs = {12} md = {8}>
-            <Map/>
-          </Grid>
+  return (
+    <>
+      <CssBaseline />
+      <Header /*onPlaceChanged={onPlaceChanged} onLoad={onLoad}*/ />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+
+        <Grid item xs={12} md={4}>
+          <Lists />
         </Grid>
-      </>
-   );
+
+
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+
+
+      </Grid>
+    </>
+  );
 }
 export default App;
 
+
+
+/*
+
+  return (
+    <>
+      <CssBaseline />
+      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List
+            isLoading={isLoading}
+            childClicked={childClicked}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map
+            setChildClicked={setChildClicked}
+            setBounds={setBounds}
+            setCoords={setCoords}
+            coords={coords}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default App;
+*/
--- Project1/travel-advisor/src/components/Map/Map.jsx
+++ Project1/travel-advisor/src/components/Map/Map.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { Paper, Typography , useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
-import Rating from '@material-ui/lab'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
 
-import useStyles from './styles';
+// import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
+
 
 const Map = () => {
-    return ( 
-        <h1></h1>
-     );
+
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0 };
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key : 'AIzaSyBE9m8cy1XDB-hcG5ej7iLWosJatX7bIQE' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+    );
 }
- 
+
 export default Map;
